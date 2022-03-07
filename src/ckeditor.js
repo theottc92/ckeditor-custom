a77e62d4418870a1efd59bf2b651e0e89bf73645
theottc92 <theo@you-agency.com>
2022-03-07 23:57:16 +0800

    added strapi upload plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,22 +7,31 @@ import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
 import BlockToolbar from "@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar.js";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder.js";
+import CKFinderUploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js";
 import CloudServices from "@ckeditor/ckeditor5-cloud-services/src/cloudservices.js";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import GeneralHtmlSupport from "@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
+import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed.js";
 import Image from "@ckeditor/ckeditor5-image/src/image.js";
 import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
 import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
 import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
 import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
 import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock.js";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
 import Link from "@ckeditor/ckeditor5-link/src/link.js";
 import List from "@ckeditor/ckeditor5-list/src/list.js";
 import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js";
+import MediaEmbedToolbar from "@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js";
 import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableCaption from "@ckeditor/ckeditor5-table/src/tablecaption.js";
+import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
 import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
 import { StrapiUploadAdapter } from "@gtomato/ckeditor5-strapi-upload-plugin";
@@ -35,22 +44,31 @@ Editor.builtinPlugins = [
   BlockQuote,
   BlockToolbar,
   Bold,
+  CKFinder,
+  CKFinderUploadAdapter,
   CloudServices,
   Essentials,
+  GeneralHtmlSupport,
   Heading,
+  HtmlEmbed,
   Image,
   ImageCaption,
   ImageStyle,
   ImageToolbar,
   ImageUpload,
   Indent,
+  IndentBlock,
   Italic,
   Link,
   List,
   MediaEmbed,
+  MediaEmbedToolbar,
   Paragraph,
   PasteFromOffice,
   Table,
+  TableCaption,
+  TableCellProperties,
+  TableProperties,
   TableToolbar,
   TextTransformation,
   StrapiUploadAdapter,
@@ -62,7 +80,24 @@ Editor.defaultConfig = {
     items: ["bold", "italic", "link"],
   },
   language: "en-gb",
-  blockToolbar: [],
+  blockToolbar: [
+    "heading",
+    "|",
+    "bulletedList",
+    "numberedList",
+    "|",
+    "indent",
+    "outdent",
+    "|",
+    "imageUpload",
+    "blockQuote",
+    "mediaEmbed",
+    "htmlEmbed",
+    "insertTable",
+    "|",
+    "undo",
+    "redo",
+  ],
   image: {
     toolbar: [
       "imageTextAlternative",
@@ -72,7 +107,13 @@ Editor.defaultConfig = {
     ],
   },
   table: {
-    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+    contentToolbar: [
+      "tableColumn",
+      "tableRow",
+      "mergeTableCells",
+      "tableCellProperties",
+      "tableProperties",
+    ],
   },
 };
 
